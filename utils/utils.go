3cc878ba2731186172899bebec10726471f37ab1
Marco Agner <marcoagner.dev@gmail.com>
2014-10-26 16:12:34 -0200

    go fmt the code

--- utils/utils.go
+++ utils/utils.go
@@ -1,20 +1,20 @@
 package utils
 
-import ( . "fmt" )
+import (
+	. "fmt"
+)
 
 func ThrowMsg(msg int) {
-    /* Availables Messages:
-        0 - Default Msg
-        1 - Invalid Command Error
-        2 - Missing Parameter
-    */
+	/* Availables Messages:
+	   0 - Default Msg
+	   1 - Invalid Command Error
+	   2 - Missing Parameter
+	*/
 
-    messages := [...]string{
-        "Welcome Batman!\n\n version: v0.1.0\n author: Raphael Amorim\n ",
-        "Hey Batman, I've founded a error!\n\n Error: Invalid command\n Suggestion: use `robin help` to see all commands\n",
-        "Hey Batman, please sent a valid parameter!" }
+	messages := [...]string{
+		"Welcome Batman!\n\n version: v0.1.0\n author: Raphael Amorim\n ",
+		"Hey Batman, I've founded a error!\n\n Error: Invalid command\n Suggestion: use `robin help` to see all commands\n",
+		"Hey Batman, please sent a valid parameter!"}
 
-    Println("[Robin]", messages[msg])
+	Println("[Robin]", messages[msg])
 }
-
-
